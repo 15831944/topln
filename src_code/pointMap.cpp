f6635cea0c92c7a7326de4dd1199974ea53987ac
zhouhuagang <jose668@sina.com>
2018-07-14 12:26:46 +0800

    ignore it addings

--- src_code/pointMap.cpp
+++ src_code/pointMap.cpp
@@ -426,9 +426,9 @@ CPointMap::findPointPairs(IN const double dist,OUT vector<pair<void*,void*>>& po
 {
 	map<double,SXData,dblcmp>::iterator itrxFirst = m_mapXcoord.begin(); //取出的x列，用来对比；
 	map<double,SXData,dblcmp>::iterator itrxNext;    //取出的x+n列，用来被对比; x + n > x;
-	map<double,SYData,dblcmp>::iterator itry;  //取得y坐标;
+	map<double,SYData,dblcmp>::iterator itry;  //取得y坐标;   
 	
-	//three level loops;
+	//three level loops;   
 	//取得x坐标;
 	double mx = 0;
 	double my = 0;
