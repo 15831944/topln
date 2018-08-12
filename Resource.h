957b748abd85024d3a66014f47572d90f203c98d
ganghua <37436803+zhouhuagang@users.noreply.github.com>
2018-08-12 21:32:43 +0800

    a little

--- Resource.h
+++ Resource.h
@@ -4,6 +4,9 @@
 //
 #define IDS_PROJNAME                    100
 #define IDD_DIALOG1                     101
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_BUTTON3                     1003
 
 // Next default values for new objects
 // 
@@ -11,7 +14,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- src_code/pointMap.h
+++ src_code/pointMap.h
@@ -53,7 +53,7 @@ eg£º Date:	Author: 	Modification:
 #include  "opt.h"
 #include "elapsetime.h"
 #include "basefunc.h"
-#include "D:\github\topln\topln\findDist.h"
+#include "..\findDist.h"
 
 using namespace std;
 
