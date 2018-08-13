30b79a7fbf8544e79895a529b5b97c94293fb355
ganghua <37436803+zhouhuagang@users.noreply.github.com>
2018-08-13 22:04:29 +0800

    a little addings

--- src_code/pointMap.cpp
+++ src_code/pointMap.cpp
@@ -436,6 +436,23 @@ CPointMap::insert(IN const AcGePoint3d pt,IN const int ptIndex,IN void* voidPtr)
 }
 
 
+//删除一个元素
+//分两层删除，先找到x，然后删除y；然后判断x是否为空，空就删除x;
+bool
+CPointMap::erase(IN const double x,IN const double y)
+{
+	map<double,SXData,dblcmp>::iterator itrX = m_mapXcoord.begin();
+	//find SXData
+	itrX = m_mapXcoord.find(x);
+	if(itrX == m_mapXcoord.end())
+	{
+		return false;  //没找到，不删除；返回false；	
+	}
+
+	//找y;
+}
+
+
 //查找：查找2次，第一次查x，第二次查y;
 //因为key是double类型，所以不能依靠等于运算来查找，应该依靠upper_bound和lower_bound;
 //返回：1.是否查找到了;
